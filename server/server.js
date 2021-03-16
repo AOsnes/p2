fe0c1c84527b1166daac27d4028e6fe541f0b4d5
Sigurd <j.skadborg00@gmail.com>
2021-03-16 13:59:28 +0100

    Server middleware for logging requests

--- server/server.js
+++ server/server.js
@@ -4,6 +4,19 @@ const app = express();
 //TODO: lav dotenv fil
 const port = process.env.PORT || 5000
 
+let logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} requested at: ${req.requestTime} (DATE object)`);
+    next();
+}
+
+let requestTime = (req, res, next) => {
+    req.requestTime = Date.now();
+    next();
+}
+
+app.use(requestTime);
+app.use(logger);
+
 app.get('/', (req, res) =>{
     res.send('Hello world!')
 })
