97cc28aed01ff7bce23caa61db5ac851bde271e9
Sigurd <j.skadborg00@gmail.com>
2021-03-16 13:55:44 +0100

    Server now listens on port 5000

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+//TODO: lav dotenv fil
+const port = process.env.PORT || 5000
+
 app.get('/', (req, res) =>{
     res.send('Hello world!')
+})
+
+app.listen(port, () =>{
+    console.log(`Server is listening on port ${port}`)
 })
