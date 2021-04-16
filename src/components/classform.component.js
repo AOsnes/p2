7e0dbd38bf1035a9e76ba498d0e5e937a933fe61
VictorDore <vdha20@student.aau.dk>
2021-04-16 14:09:09 +0200

    noMatchError component test + deletion of classform

--- src/components/classform.component.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class header extends Component {
-    render(){
-        return(
-            <a href="http://localhost:5000/classes">test</a>
-        );
-    }
-}
