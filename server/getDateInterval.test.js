7cd50397a2c2d2f6e67385e661f151720ebf41cf
Arthur Osnes Gottlieb <arthurosnes@gmail.com>
2021-04-20 15:31:22 +0200

    Update getDateInterval.test.js

--- server/getDateInterval.test.js
+++ server/getDateInterval.test.js
@@ -1,9 +1,5 @@
 import { getDateInterval} from './server';
 
-test('getDateIntervalMonday1', () =>{
-    let start = new Date(2021, 3, 19, 0, 0, 0);
-    let end = new Date(2021, 3, 19, 23, 59, 59);
-    let expectedObject = {start, end};
-    expect(getDateInterval(new Date (2021, 3, 19), '1')).toEqual(expectedObject);
-})
+const oneDayInterval = jest.fn();
+
 
