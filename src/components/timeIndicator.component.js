d0d3206366e8599bd3e88e89474b7428a900ffe5
Mike Jensen <mike.jensen.at@gmail.com>
2021-05-05 14:26:49 +0200

    Fix name and CSS

--- src/components/timeIndicator.component.js
+++ src/components/timeIndicator.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-export default class NoMatchError extends Component {
-    render(){
+export default class TimeIndicator extends Component {
+     render(){
         return(
             <div className="timeIndicator"></div>
         )
