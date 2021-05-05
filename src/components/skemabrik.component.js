4cd1903c53107acd06692df18bab39246f6d34e3
Arthur Osnes Gottlieb <arthurosnes@gmail.com>
2021-05-05 10:03:01 +0200

    Revert "Revert "Blur behind modal added when clicked""
    This reverts commit 558c76c52dba5a05c0f91fb748003256d2819261.

--- src/components/skemabrik.component.js
+++ src/components/skemabrik.component.js
@@ -39,14 +39,16 @@ export default class Skemabrik extends Component {
         this.setState({
             showSkemabrikModal: false
         })
+        document.getElementsByClassName('skemaContainer')[0].classList.remove('blur-filter')
     }
 
     /* Whenever the skemabrik is pressed, reverse the state */
     onSkemaClick(e){
         this.setState(prevState => ({
             showSkemabrikModal: !prevState.showSkemabrikModal
-            }));
-        }
+        }));
+        this.state.showSkemabrikModal ?  document.getElementsByClassName('skemaContainer')[0].classList.remove('blur-filter') : document.getElementsByClassName('skemaContainer')[0].classList.add('blur-filter')
+    }
 
     render(){
         const subject = this.props.skemabrik.subject;
