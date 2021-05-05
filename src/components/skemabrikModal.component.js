2cee9b24371bfadf541672f6124761d19ced3b53
VictorDore <vdha20@student.aau.dk>
2021-05-05 11:09:09 +0200

    Time and scrollbar added for description

--- src/components/skemabrikModal.component.js
+++ src/components/skemabrikModal.component.js
@@ -17,15 +17,19 @@ export default class SkemabrikModal extends Component{
     render(){
         const details = this.props.skemabrikContext.description;
         const classes = this.props.skemabrikContext.class;
+        const subject = this.props.skemabrikContext.subject;
+        const startTime = new Date(this.props.skemabrikContext.startTime);
+        const endTime = new Date(this.props.skemabrikContext.endTime);
         return( ReactDOM.createPortal(
-                <div className="detailsModal">
+                <div className={`detailsModal ${subject}`}>
                     <div onClick={this.handleClick} className="close">&#10006;</div>
-                    <div className="skemabrikModalText textLeft">{details}</div>
+                    <div className="skemabrikModalText textCenter">{this.props.toHHMM(startTime)} - {this.props.toHHMM(endTime)}</div>
+                    <div className="skemabrikModalText detailsText textLeft">{details}</div>
                     <UserContext.Consumer>
                         {user => {
                             if(user.role === 'teacher')
                                 return(
-                                    <p className="skemabrikModalText textRight"> Klasse: {classes}</p>
+                                    <p className="skemabrikModalText textLeft"> Klasse: {classes}</p>
                                 )
                         }}
                     </UserContext.Consumer>
