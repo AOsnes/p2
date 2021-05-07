f50b08fd9c383c4a3228f17489725843d0621cdf
Andreas ahlbeck <aahlbe20@student.aau.dk>
2021-05-07 09:31:41 +0200

    ToggleView Button

--- src/components/dagsvisning.component.js
+++ src/components/dagsvisning.component.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { BiCheckbox } from 'react-icons/bi';
+import { UserContext } from '../UserContext';
 
-export default class Dagsvisning extends Component {
+export default class Dagsvisning extends Component{
+    static contextType= UserContext;
+    constructor(props,context){
+        super(props);
+        this.state= { isToggleOn: context.role!=='teacher' ? true: false};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+    }
 
     render(){
-        return(
-            <div className="toggleVisningKnap">123</div>
-        )
-        }
-    
+        return (
+            <div className="toggleVisning">
+                <button className="button"
+                    onClick={this.handleClick}>
+                    {this.state.isToggleOn ? '1 dags visning' : '5 dags visning'}
+                </button>
+                {/*this.state.isToggleOn ? <Placeholder dayview/> : Placeholder Fivedayview*/}
+            </div>
+        );
+    }
 }
